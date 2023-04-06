ef2f095cba224530e673cb047f9abf919062943c
Shadume <delaportemarjorie@yahoo.fr>
2023-04-06 16:25:48 +0200

    bdd creation and adding stations to corresponding table

---
+++ bdd.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+connexion = sqlite3.connect("bdd.db")
+curseur = connexion.cursor()
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS gare (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nom TEXT NOT NULL,
+                    frequentation_2019 INTEGER NOT NULL,
+                    frequentation_2020 INTEGER NOT NULL,
+                    frequentation_2021 INTEGER NOT NULL
+                )
+                """)
+
+connexion.commit()
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS objet_trouve (
+                    gare_id INTEGER,
+                    journee_id INTEGER,
+                    type TEXT NOT NULL,
+                    FOREIGN KEY(gare_id) REFERENCES gare(id),
+                    FOREIGN KEY(journee_id) REFERENCES journee(id)
+                )
+                """)
+
+connexion.commit()
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS journee (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date DATE NOT NULL,
+                    temperature INTEGER NOT NULL
+                )
+                """)
+
+connexion.commit()
+connexion.close()
